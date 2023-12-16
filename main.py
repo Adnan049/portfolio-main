0d4b242095dc7a7ee7ffa24e4822131c753c8189
Adnan <leylajafarli.email@gmail.com>
2023-12-16 16:39:38 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#Импорт
+from flask import Flask, render_template,request, redirect
+
+
+
+app = Flask(__name__)
+
+#Запуск страницы с контентом
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+#Динамичные скиллы
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    return render_template('index.html', button_python=button_python, my_url="https://github.com/Adnan049/diary-main-02-12-23-main")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
